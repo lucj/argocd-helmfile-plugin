2788c137e22b17ec1eb06fcfa292436b7a7cae69
Luc Juggery <luc.juggery@gmail.com>
2023-04-16 19:39:42 +0200

    Dump dependencies versions

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-# Init builder
-docker buildx create --use
-
-# Build for multiple arch (almost needed since Mac M1)
-docker buildx build --platform linux/arm64/v8,linux/amd64 -t lucj/argocd-plugin-helmfile:v0.0.10 . --push
